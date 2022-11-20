9323751283afec579709aec1048f8d6ffaf79718
pskendre <pskendre03@gmail.com>
2022-11-20 15:13:11 +0530

    arithmetic file is added

---
+++ ArithmeticOperation.java
@@ -0,0 +1,21 @@
+
+public class ArithmeticOperation {
+	public static void main(String args[])
+	{
+		int a = 5;
+		int b = 2;
+		int ans= a+b;
+		System.out.println(ans);
+		System.out.println(a+b);
+		System.out.println("ans=" + (a+b));
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		System.out.println((a*b)/(a-b));
+		byte c= 10;
+				c=(byte) (c+1);
+				System.out.println("c=" + c);
+	}
+
+}
